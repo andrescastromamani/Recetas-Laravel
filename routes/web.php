7d97924a0ac90e6d25f85d97fac2d8386e609539
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-09-09 16:09:00 -0400

    We add a RecipesController and we redirect to route recipes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecipesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/recetas', [RecipesController::class, 'index'])->name('recipes');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
