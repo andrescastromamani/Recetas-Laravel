a26ae78de3f59759f6bd9862c4be632fa3cf6761
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-10-04 15:46:59 -0400

    We show more details of the recipe

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -34,10 +34,17 @@
                                 <img src="/storage/{{$recipe->image}}" class="card-img-top" alt="image recipe">
                                 <div class="card-body">
                                     <h5 class="card-title text-center">{{$recipe->title}}</h5>
-                                    <p class="card-text text-center">Some quick example text to build on the card title
-                                        and make up
-                                        the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-outline-info d-block">Go somewhere</a>
+                                    <div class="d-flex justify-content-between">
+                                        <p class="text-info font-weight-bold">
+                                            <date-recipe>
+                                                date="{{$recipe->created_at}}"
+                                            </date-recipe>
+                                        </p>
+                                        <p>{{count($recipe->likeuser)}} les gusto</p>
+                                    </div>
+                                    <p class="card-text text-center">{{Str::words(strip_tags($recipe->preparation),15)}}</p>
+                                    <a href="{{route('recipes.show',$recipe->id)}}"
+                                       class="btn btn-outline-info d-block">Ver Receta</a>
                                 </div>
                             </div>
                         </div>
