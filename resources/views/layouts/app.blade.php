9e7690475032217c38d0b22761b36e5c3871283a
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-10-06 15:09:20 -0400

    We create a CategoryProvider andwe show in the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,24 @@
             </div>
         </div>
     </nav>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    @foreach($categories as $category)
+                        <li class="nav-item">
+                            <a class="nav-link font-weight-bold text-uppercase" aria-current="page"
+                               href="#">{{$category->name}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </nav>
     <main class="py-4">
         @yield('content')
     </main>
